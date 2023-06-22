a23a0053279f5ee049e7835ef21df84b62552720
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2023-06-22 09:43:02 +0200

    Some modifications

--- src/app.rs
+++ src/app.rs
@@ -1,22 +1,27 @@
+
+use std::cell::RefCell;
+use std::borrow::BorrowMut;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
-    #[serde(skip)]
     value: f32,
+    is_win_open: bool,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            value: 0.0,
+            is_win_open: true,
         }
+    }
+}
+
+impl TemplateApp {
+    pub fn name() -> &'static str {
+        "Test application"
     }
 }
 
@@ -45,37 +50,30 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+        egui::TopBottomPanel::top("top_bar").show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                egui::widgets::global_dark_light_mode_switch(ui);
+                ui.separator();
 
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+                //ui.menu_button("😵 Test Menu button", |ui| {
+                //    ui.set_style(ui.ctx().style()); // ignore the "menu" style set by `menu_button`
+                //});
 
-        #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        _frame.close();
-                    }
-                });
+                //ui.menu_button("👉 Test menu button", |ui| {
+                //    ui.set_style(ui.ctx().style());
+                //    ui.button("oops");
+                //});
+
+                if ui.selectable_label(false, "Test").clicked() {
+                    println!("OOps");
+                }
             });
+
         });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
             ui.heading("Side Panel");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 ui.horizontal(|ui| {
@@ -95,22 +93,28 @@ impl eframe::App for TemplateApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
+            ui.heading(format!("{} - test app", Self::name()));
+            ui.label("This is hardworking");
             egui::warn_if_debug_build(ui);
+
+            ui.separator();
+
+            ui.horizontal(|ui| {
+                ui.checkbox(&mut self.is_win_open, "Is window opened ?")
+                    .on_hover_text("Click to open window!");
+            })
         });
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally choose either panels OR windows.");
-            });
+        if true {
+            egui::Window::new("Window")
+                .open(&mut self.is_win_open)
+                .show(ctx, |ui| {
+                    ui.label("Windows can be moved by dragging them.");
+                    ui.label("They are automatically sized based on contents.");
+                    ui.label("You can turn on resizing and scrolling if you like.");
+                    ui.label("You would normally choose either panels OR windows.");
+                })
+            ;
         }
     }
 }
